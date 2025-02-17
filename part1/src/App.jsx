8ff5893eb3269ad1135d6bc477755bb91f1169a7
iFangg <i.Fangg@proton.me>
2025-02-17 15:54:01 +1100

    Implemented buttons to modify a counter

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,46 @@
 import { useState } from 'react'
 
-const App = () => {
-
-  const [ counter, setCounter ] = useState(0)
-
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
-
+const Display = ({ counter }) => {
   return (
     <div>{counter}</div>
   )
+}
+
+const Button = ({ onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+
+    const [ counter, setCounter ] = useState(0)
+
+    // const handleClick = () => {
+    //     console.log('clicked')
+    // }
+
+
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+
+    return (
+        <div>
+            <Display counter={counter}></Display>
+            <Button
+                onClick={increaseByOne}
+                text='plus'
+            />
+            <Button onClick={setToZero} text='zero' />
+            <Button
+                onClick={decreaseByOne}
+                text='minus'
+            />
+        </div>
+    )
 }
 
 export default App
