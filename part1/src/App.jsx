67c8293c7f4cd7ede185cb23fb8d8b351ed9b43e
iFangg <i.Fangg@proton.me>
2025-02-17 16:44:19 +1100

    Refactored buttons

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,59 +1,26 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
 
-const Button = ({ onClick, text}) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+const App = (props) => {
+    const [value, setValue] = useState(10)
 
-const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-    const [total, setTotal] = useState(0)
-
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        const updated = left + 1
-        setLeft(updated)
-        setTotal(updated + right)
-    }
-
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        const updated = right + 1
-        setRight(updated)
-        setTotal(left + updated)
+    const setToValue = (newValue) => {
+        console.log('value now', newValue)
+        setValue(newValue)
     }
 
     return (
         <div>
-        {left}
-        <Button onClick={handleLeftClick} text='left' />
-        <Button onClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks}/>
-        <p>total: {total}</p>
+            <p>{value}</p>
+
+            <Button onClick={() => setToValue(1000)} text="thousand" />
+            <Button onClick={() => setToValue(0)} text="reset" />
+            <Button onClick={() => setToValue(value + 1)} text="increment" />
         </div>
     )
 }
+
 export default App
