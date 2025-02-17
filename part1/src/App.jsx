cb2232e147eaaa0ec1aaeb2af1e9ee63c7a67a00
iFangg <i.Fangg@proton.me>
2025-02-17 16:36:54 +1100

    refactored slightly more complex state handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
@@ -15,24 +24,36 @@ const Button = ({ onClick, text}) => {
 }
 
 const App = () => {
-    const [clicks, setClicks] = useState({
-        left: 0, right: 0
-    })
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
 
-    const handleLeftClick = () =>
-        setClicks({ ...clicks, left: clicks.left + 1 })
 
-    const handleRightClick = () =>
-        setClicks({ ...clicks, right: clicks.right + 1 })
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updated = left + 1
+        setLeft(updated)
+        setTotal(updated + right)
+    }
+
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        const updated = right + 1
+        setRight(updated)
+        setTotal(left + updated)
+    }
 
     return (
         <div>
-        {clicks.left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks}/>
+        <p>total: {total}</p>
         </div>
     )
 }
-
 export default App
