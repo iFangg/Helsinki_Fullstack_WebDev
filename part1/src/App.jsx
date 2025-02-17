17e85dea75458fb1c236e84796b5ec3d37a51bb8
iFangg <i.Fangg@proton.me>
2025-02-17 16:03:12 +1100

    Implemented slightly more complex state handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -15,30 +15,22 @@ const Button = ({ onClick, text}) => {
 }
 
 const App = () => {
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
 
-    const [ counter, setCounter ] = useState(0)
+    const handleLeftClick = () =>
+        setClicks({ ...clicks, left: clicks.left + 1 })
 
-    // const handleClick = () => {
-    //     console.log('clicked')
-    // }
-
-
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
+    const handleRightClick = () =>
+        setClicks({ ...clicks, right: clicks.right + 1 })
 
     return (
         <div>
-            <Display counter={counter}></Display>
-            <Button
-                onClick={increaseByOne}
-                text='plus'
-            />
-            <Button onClick={setToZero} text='zero' />
-            <Button
-                onClick={decreaseByOne}
-                text='minus'
-            />
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
         </div>
     )
 }
